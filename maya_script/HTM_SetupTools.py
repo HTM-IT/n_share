390397430fa3d3d3ee176febfe7881dcee4db4f8
HitomuIto <86769670+HitomuIto@users.noreply.github.com>
2025-05-25 16:48:29 +0900

    Update HTM_SetupTools.py

--- maya_script/HTM_SetupTools.py
+++ maya_script/HTM_SetupTools.py
@@ -12,8 +12,10 @@ from shiboken2 import wrapInstance
 
 import maya.cmds as mc
 import maya.api.OpenMaya as om2
-import maya.OpenMayaAnim as oma
+import maya.api.OpenMayaAnim as oma2
 import maya.OpenMaya as om
+import maya.OpenMayaAnim as oma
+
 from maya.OpenMayaUI import MQtUtil
 from maya import OpenMayaUI as omui
 from maya.app.general.mayaMixin import MayaQWidgetBaseMixin
@@ -128,34 +130,72 @@ class HTM_SetupTools(MayaQWidgetBaseMixin, QMainWindow):
         main_layout.addWidget(gb_fj)
 
         # ----------------------------------------------
-        # ジョイントの再初期化
-        gb_rj = QGroupBox(u'ジョイントの再初期化')
-        vbl_rj = QVBoxLayout()
-        vbl_rj.setContentsMargins(2, 2, 2, 2)
-        vbl_rj.setSpacing(4)
+        # バインドポーズ関連
+        gb_bp = QGroupBox(u'バインドポーズ')
+        vbl_bp = QVBoxLayout()
+        vbl_bp.setContentsMargins(2, 2, 2, 2)
+        vbl_bp.setSpacing(4)
 
-        pb_rj = QPushButton(u'ジョイントの再初期化')
-        pb_rj.clicked.connect(HTM_ReinitializeSkinnedJoint.reinitialize)
-        pb_rj.setFixedHeight(30)
-        vbl_rj.addWidget(pb_rj)
+        pb_bp = QPushButton(u'ジョイントの再初期化')
+        pb_bp.clicked.connect(HTM_ReinitializeSkinnedJoint.reinitialize)
+        pb_bp.setFixedHeight(30)
+        vbl_bp.addWidget(pb_bp)
 
-        gb_rj.setLayout(vbl_rj)
-        main_layout.addWidget(gb_rj)
-
-        # ----------------------------------------------
-        # バインドポーズの作り直し
-        gb_rbp = QGroupBox(u'バインドポーズを一つに')
-        vbl_rbp = QVBoxLayout()
-        vbl_rbp.setContentsMargins(2, 2, 2, 2)
-        vbl_rbp.setSpacing(4)
-
-        pb_rbp = QPushButton(u'現在のジョイントの状態で\nバインドポーズを作り直す')
+        pb_rbp = QPushButton(u'バインドポーズを1つにする\n(現在のジョイントの状態をバインドポーズに)')
         pb_rbp.clicked.connect(HTM_RecreateBindPose.recreate_bind_pose)
         pb_rbp.setFixedHeight(40)
-        vbl_rbp.addWidget(pb_rbp)
+        vbl_bp.addWidget(pb_rbp)
 
-        gb_rbp.setLayout(vbl_rbp)
-        main_layout.addWidget(gb_rbp)
+        gb_bp.setLayout(vbl_bp)
+        main_layout.addWidget(gb_bp)
+
+        # ----------------------------------------------
+        # バインドポーズ関連
+        gb_dh = QGroupBox(u'ヒストリ整理')
+        vbl_dh = QVBoxLayout()
+        vbl_dh.setContentsMargins(2, 2, 2, 2)
+        vbl_dh.setSpacing(4)
+
+        pb_rbp = QPushButton(u'ヒストリの削除\n(バインド情報は残す)')
+        pb_rbp.clicked.connect(HTM_DeleteHistoryWithoutSC.del_history_without_sc)
+        pb_rbp.setFixedHeight(40)
+        vbl_dh.addWidget(pb_rbp)
+
+        gb_dh.setLayout(vbl_dh)
+        main_layout.addWidget(gb_dh)
+
+        # ----------------------------------------------
+        # バインドポーズ関連
+        gb_jo = QGroupBox(u'ジョイントの方向づけ')
+
+        # コンボボックスオブジェクトの生成
+        self.cb_jo_prim = QComboBox(self)
+        self.cb_jo_prim.addItems(['X', 'Y', 'Z'])
+        self.cb_jo_sec = QComboBox(self)
+        self.cb_jo_sec.addItems(['X', 'Y', 'Z', 'Bend'])
+        self.cb_jo_sec.setCurrentIndex(1)
+        self.cb_jo_w = QComboBox(self)
+        self.cb_jo_w.addItems(['X', 'Y', 'Z'])
+        self.cb_jo_w.setCurrentIndex(1)
+        self.chb_inverse = QCheckBox(u'反転')
+        pb_jo = QPushButton(u'ジョイントの方向づけ')
+        pb_jo.clicked.connect(self.joint_orient_clbk)
+
+        gl_jo = QGridLayout()
+        gl_jo.setContentsMargins(2, 2, 2, 2)
+        gl_jo.setSpacing(4)
+        gl_jo.addWidget(QLabel(u'プライマリ軸 :'), 0, 0)
+        gl_jo.addWidget(self.cb_jo_prim, 0, 1)
+        gl_jo.addWidget(QLabel(u'セカンダリ軸 :'), 1, 0)
+        gl_jo.addWidget(self.cb_jo_sec, 1, 1)
+        gl_jo.addWidget(QLabel(u'セカンダリ軸のワールド方向 :'), 2, 0, 1, 2)
+        gl_jo.addWidget(self.cb_jo_w, 3, 0)
+        gl_jo.addWidget(self.chb_inverse, 3, 1)
+        gl_jo.addWidget(pb_jo, 4, 0, 1, 2)
+
+
+        gb_jo.setLayout(gl_jo)
+        main_layout.addWidget(gb_jo)
 
         # ----------------------------------------------
         # メインレイアウト処理
@@ -183,6 +223,33 @@ class HTM_SetupTools(MayaQWidgetBaseMixin, QMainWindow):
             HTM_FreezeJoint.search_unfreezed_joint(mode='rotation')
         else:
             HTM_FreezeJoint.search_unfreezed_joint(mode='orientation')
+
+    def joint_orient_clbk(self):
+        # Get primary axis, secondary axis and bend mode flag
+        prm_axis = self.cb_jo_prim.currentText().lower()
+        sec_axis = self.cb_jo_sec.currentText().lower()
+        if prm_axis == sec_axis:
+            om2.MGlobal.displayError(u'プライマリ軸とセカンダリ軸はそれぞれ別々の軸を設定してください')
+            return
+
+        bend_direction = False
+        if sec_axis == 'bend':
+            axis_table = {'x':'z', 'y':'x', 'z':'y'}
+            sec_axis = axis_table[prm_axis]
+            bend_direction = True
+
+        # セカンダリ軸のワールド方向の取得
+        sec_target = self.cb_jo_w.currentText().lower()
+
+        # ワールド方向の反転フラグ取得
+        sec_target_inv = self.chb_inverse.isChecked()
+        if sec_target_inv:
+            sec_target = '-' + sec_target
+
+        HTM_JointOrient.joint_orient(bend_direction, prm_axis=prm_axis,
+                                   sec_axis=sec_axis, sec_target=sec_target)
+        #self.orient_func.joint_orient(bend_direction, prm_axis=prm_axis,
+        #                           sec_axis=sec_axis, sec_target=sec_target)
 
 
 class HTM_FreezeJoint:
@@ -320,8 +387,8 @@ class HTM_ReinitializeSkinnedJoint:
 
 
 class HTM_RecreateBindPose:
-    @staticmethod
-    def get_all_root_joints():
+    @classmethod
+    def get_all_root_joints(cls):
         # ルートジョイント取得用再帰関数
         def get_root_joint(joint, processed):
             parent_temp = mc.listRelatives(joint, p=True, type='joint', f=True)
@@ -332,6 +399,8 @@ class HTM_RecreateBindPose:
             return get_root_joint(parent_temp[0], processed)
 
         joints = mc.ls(sl=True, type='joint', l=True)
+        if not joints:
+            om2.MGlobal.displayError(u'ジョイントの最低1つは選択して実行してください')
 
         processed = []
         root_joints = []
@@ -345,7 +414,7 @@ class HTM_RecreateBindPose:
 
     @classmethod
     def recreate_bind_pose(cls):
-        roots = get_all_root_joints()
+        roots = cls.get_all_root_joints()
 
         for root in roots:
             childlen = mc.listRelatives(root, ad=True, type='joint', f=1)
@@ -355,6 +424,238 @@ class HTM_RecreateBindPose:
             # dagPoseノードの削除と作り直し
             mc.delete(dagpose)
             mc.dagPose(root, bp=True, save=True)
+
+
+class HTM_DeleteHistoryWithoutSC:
+    @classmethod
+    def get_sc(cls, obj_name_str):
+        sc = mc.ls(mc.listHistory(obj_name_str, pdo=True), type='skinCluster')[0]
+        sel_sc = om2.MGlobal.getSelectionListByName(sc)
+        mo_sc= sel_sc.getDependNode(0)
+        fn_sc = oma2.MFnSkinCluster(mo_sc)
+        return sc, fn_sc
+
+    @classmethod
+    @undo_ctx
+    def del_history_without_sc(cls):
+        sel = om2.MGlobal.getActiveSelectionList()
+        it_sel = om2.MItSelectionList(sel)
+        selection = mc.ls(sl=True)
+
+        for it_s in it_sel:
+            dag, comp = it_s.getComponent()
+
+            # トランスフォームノード以外が選択されていたらスルー
+            # シェイプがない場合もスルー
+            if not dag.hasFn(om2.MFn.kTransform):
+                continue
+
+            fn_trans = om2.MFnTransform(dag)
+            has_shape = False
+            for i in range(fn_trans.childCount()):
+                child = fn_trans.child(i)
+                if child.hasFn(om2.MFn.kMesh):
+                    has_shape = True
+            if not has_shape:
+                continue
+
+            # シェイプの取得
+            dp_shape = dag.extendToShape()
+            obj_name_str = dag.fullPathName()
+
+            # スキンクラスター関連情報取得
+            sc, fn_sc = cls.get_sc(obj_name_str)
+
+            infs = mc.skinCluster(sc, q=True, inf=True)
+            max_inf = mc.getAttr(sc + '.maxInfluences')
+            normalize_weights = mc.getAttr(sc + '.normalizeWeights')
+            maintain_max_inf = mc.getAttr(sc + '.maintainMaxInfluences')
+
+            # ウェイト保存
+            weights, num_inf = fn_sc.getWeights(dp_shape, comp)
+
+            # ヒストリ削除
+            mc.skinCluster(obj_name_str, e=True, ub=True)
+            mc.delete(obj_name_str, ch=True)
+            mc.skinCluster(obj_name_str, infs, tsb=True, mi=max_inf,
+                           omi=maintain_max_inf, nw=normalize_weights)
+
+            # バインド情報復旧
+            _, fn_sc_new = cls.get_sc(obj_name_str)
+            inf_ids = om2.MIntArray(range(num_inf))
+            fn_sc_new.setWeights(dp_shape, comp, inf_ids, weights)
+
+        om2.MGlobal.setActiveSelectionList(sel)
+
+
+class HTM_JointOrient:
+    @staticmethod
+    def get_n_vector(joint_0, joint_1, joint_2):
+        """
+        Param:
+            pos_0: float list or MPoint list
+            pos_1: float list or MPoint list
+            pos_2: float list or MPoint list
+        """
+        pos_0 = om2.MPoint(mc.xform(joint_0, q=True, ws=True, t=True))
+        pos_1 = om2.MPoint(mc.xform(joint_1, q=True, ws=True, t=True))
+        pos_2 = om2.MPoint(mc.xform(joint_2, q=True, ws=True, t=True))
+
+        vector_0 = (pos_1 - pos_0).normal()
+        vector_1 = (pos_2 - pos_1).normal()
+
+        n_vector = (vector_0 ^ vector_1).normal()
+
+        return n_vector
+
+    @staticmethod
+    def get_aim_vector(joint_0, joint_1):
+        """
+        Param:
+            jiont_0(str): fullpath
+            jiont_1(str): fullpath
+        """
+        pos_base = om2.MPoint(mc.xform(joint_0, q=True, ws=True, t=True))
+        pos_aim = om2.MPoint(mc.xform(joint_1, q=True, ws=True, t=True))
+        vec_aim = (pos_aim - pos_base).normal()
+        return vec_aim
+
+    @staticmethod
+    def aim_transform(joint, vec_aim, vec_sec,
+                      prm_axis='x', sec_axis='z', sec_target='y'):
+        """
+        """
+        vector_table = {'x' : om2.MVector(1, 0, 0),
+                        'y' : om2.MVector(0, 1, 0),
+                        'z' : om2.MVector(0, 0, 1),
+                        '-x': om2.MVector(1, 0, 0),
+                        '-y': om2.MVector(0, 1, 0),
+                        '-z': om2.MVector(0, 0, 1)}
+
+        pos = mc.xform(joint, q=True, ws=True, t=True)
+
+        # Secondary axis world orientation
+        sec_axis_target = vector_table[sec_target]
+        vec_temp = (vec_aim ^ sec_axis_target).normal()
+        if not vec_sec:
+            vec_sec = (vec_temp ^ vec_aim).normal()
+
+        # Init primary/secondary base vecotr to calculate rotation.
+        vec_prm_base = vector_table[prm_axis]
+        vec_sec_base = vector_table[sec_axis]
+
+        # Get quaternion that can rotate "vec_prm_base" to "vec_aim".
+        quaternion_aim = om2.MQuaternion(vec_prm_base, vec_aim)
+        vec_sec_base = vec_sec_base.rotateBy(quaternion_aim)
+
+        # Get quaternion about secondary vector
+        quaternion_sec = vec_sec_base.rotateTo(vec_sec)
+
+        rot_mtx = om2.MMatrix()
+        if '-' in sec_target:
+            quaternion_inverse = om2.MQuaternion(math.radians(180), vec_aim)
+            rot_mtx = (quaternion_aim * quaternion_sec * quaternion_inverse).asMatrix()
+        else:
+            rot_mtx = (quaternion_aim * quaternion_sec).asMatrix()
+
+        parent_inv_mtx = om2.MMatrix(mc.getAttr(joint + '.parentInverseMatrix'))
+        matrix = rot_mtx * parent_inv_mtx
+
+        mc.xform(joint, ws=False, m=matrix)
+        mc.xform(joint, ws=True, t=pos)
+
+
+    @classmethod
+    @undo_ctx
+    def joint_orient(cls, bend_direction=False,
+                     prm_axis='x', sec_axis='z', sec_target='y'):
+        """
+        """
+        joints = mc.ls(sl=True, type='joint', l=True)
+        if not joints:
+            om2.MGlobal.displayError('Please select one ore more joints.')
+            return
+
+        hierarchy = True
+        if hierarchy:
+            joints_child = mc.listRelatives(joints, ad=True, type='joint', f=True)
+            if joints_child:
+                joints_child.extend(joints)
+                joints_child.reverse()
+                joints = joints_child
+
+        joints_peak = []
+        for joint in joints:
+
+            vec_sec = None
+            if bend_direction:
+                # Bend direction mode
+                parent = mc.listRelatives(joint, p=True, f=True) or []
+                child  = mc.listRelatives(joint, c=True, f=True) or []
+
+                # if there's multiple child joint
+                """
+                if len(child) > 1:
+                    om2.MGlobal.displayWarning('"{}" skipped because of multiple child joints found.')
+                    continue
+                """
+
+                # if not child joint exists
+                if not len(child):
+                    joints_peak.append(joint)
+                    continue
+
+                # if not parent joint exists
+                elif not len(parent) or parent[0] not in joints:
+                    joint_0 = joint
+                    joint_1 = child[0]
+                    joint_2 = mc.listRelatives(child, c=True, f=True)[0]
+
+                    if not joint_2:
+                        # If there's less than two joints in the selection.
+                        mc.error('There\'s no enough number of joints.')
+
+                else:
+                    joint_0 = parent
+                    joint_1 = joint
+                    joint_2 = child[0]
+
+                # Get a vector of bend axis
+                vec_sec = cls.get_n_vector(joint_0, joint_1, joint_2)
+
+            else:
+                child = mc.listRelatives(joint, c=True, f=True)
+
+                # If multiple child joints were found.
+                #if len(child) > 1:
+                #    om2.MGlobal.displayWarning('"{}" skipped because of multiple child joints found.')
+                #    continue
+
+                # If no child joints were found.
+                if not child:
+                    joints_peak.append(joint)
+                    continue
+
+            # Restore child's pos/rot
+            child_rot = []
+            child_pos = []
+            for c in child:
+                child_rot.append(mc.xform(c, q=True, ws=True, ro=True))
+                child_pos.append(mc.xform(c, q=True, ws=True, t=True))
+
+            # Execute aim_transform
+            vec_aim = cls.get_aim_vector(joint, child[0])
+            cls.aim_transform(joint, vec_aim, vec_sec, prm_axis, sec_axis, sec_target)
+
+            # Reset child pos/rot
+            for c, rot, pos in zip(child, child_rot, child_pos):
+                mc.xform(c, ws=True, ro=rot)
+                mc.xform(c, ws=True,  t=pos)
+
+        # For peak joints
+        for joint in joints_peak:
+            mc.setAttr('%s.rotate' % joint, 0, 0, 0)
+            mc.setAttr('%s.jointOrient' % joint, 0, 0, 0)
 
 
 def main():
